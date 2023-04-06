7bc11c1ce6c64be527699a23fc2d346746f4f459
otomisin <orenaiket@yahoo.com>
2023-04-06 14:36:42 +0100

    AirBnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
  script to install and conf nginx
 
-#install nginx
+#install nginx if not installed
 sudo apt-get update
 sudo apt-get install nginx -y
 
-#creating repositories
+#create repositories if not installed
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-#creating a dummy html page
+#create a dummy html page with HS
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -18,13 +18,13 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-#creating a symbolic link
+#create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-#changing directory name
+#change directory name
 sudo chown -R ubuntu:ubuntu /data/
 
-#setting up the page to be served
+#set up the page to be served
 sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
 #restart the server
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,22 +3,23 @@
 from fabric.api import local
 from datetime import datetime
 
+# Create a do pack function
 def do_pack():
     """ a method to compress a file and return it's path """
     
     """saving the current timestamp and creatinf filename"""
-    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = "versions/web_static_{}.tgz".format(time_now)
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    filePath = "versions/web_static_{}.tgz".format(current_time)
    
     try:
         """create a directory called versions"""
         local("mkdir -p versions")
          
         """create an archive file"""
-        local("tar -cvzf {} web_static/".format(file_path))
+        local("tar -cvzf {} web_static/".format(filePath))
         
         """return the path to the archive file created"""
-        return "{}".format(file_path)
+        return "{}".format(filePath)
 
         """return none if an error occurs"""
     except Exception as e:
