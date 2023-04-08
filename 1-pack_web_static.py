4b11d7b9f22a2b2efdd93824a12f088b3f03059d
otomisin <orenaiket@yahoo.com>
2023-04-08 12:54:23 +0100

    web stack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""a fabric script to create an archive file"""
+"""A fabric script to create an archiveed file"""
 from fabric.api import local
 from datetime import datetime
 
 # Create a do pack function
+
+
 def do_pack():
-    """ a method to compress a file and return it's path """
-    
-    """saving the current timestamp and creatinf filename"""
+    """Saving the current timestamp and creatinf filename"""
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     filePath = "versions/web_static_{}.tgz".format(current_time)
-   
     try:
         """create a directory called versions"""
         local("mkdir -p versions")
-         
-        """create an archive file"""
+        """create an archived file"""
         local("tar -cvzf {} web_static/".format(filePath))
-        
         """return the path to the archive file created"""
         return "{}".format(filePath)
 
