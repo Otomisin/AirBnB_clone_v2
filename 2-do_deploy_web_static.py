7bd3fd2160e858ca834ef61479b586e5ccd90566
otomisin <orenaiket@yahoo.com>
2023-04-09 05:51:19 +0100

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,33 +5,36 @@
 """
 
 from fabric.api import run, env, put
-from fabric.api import *
-import os.path
+from os.path import isfile
+import os
 
 env.hosts = ['18.209.152.209', '34.207.156.104']
 env.key_filename = "~/.ssh/school"
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
     """
         a function to deploy code and decompress it
-    """    
-    if not os.path.isfile(archive_path):
+    """
+    if not isfile(archive_path):
         return False
-    compressedFiles = archive_path.split("/")[-1]
-    without_extension = compressedFiles.split(".")[0]
-    
+
+    filename = os.path.basename(archive_path)
+    no_ext = os.path.splitext(filename)[0]
+    dest_path = '/data/web_static/releases/{}/'.format(no_ext)
+    symlink_path = '/data/web_static/current'
+
     try:
-       remote_path = "/data/web_static/releases/{}/".format(without_extension)
-       sym_link = "/data/web_static/current"
-       put(archive_path, "/tmp/")
-       run("sudo mkdir -p {}".format(remote_path))
-       run("sudo tar -xvzf /tmp/{} -C {}".format(compressedFiles, remote_path))
-       run("sudo rm /tmp/{}".format(compressedFiles))
-       run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
-       run("sudo rm -rf {}/web_static".format(remote_path))
-       run("sudo rm -rf /data/web_static/current")
-       run("sudo ln -sf {} {}".format(remote_path, sym_link))
-       return True
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest_path))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(filename, dest_path))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv {}/web_static/* {}'.format(dest_path, dest_path))
+        run('sudo rm -rf {}/web_static'.format(dest_path))
+        run('sudo rm -rf {}'.format(symlink_path))
+        run('sudo ln -s {} {}'.format(dest_path, symlink_path))
+        return True
     except Exception as e:
+        print(e)
         return False
