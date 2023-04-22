4c3499f0caa0e4f40ae9f5485c0cfd00b04f8ad4
otomisin <orenaiket@yahoo.com>
2023-04-22 13:43:17 +0100

    Deleted file: web_flask/__pycache__/0-hello_route.cpython-310.pyc

--- web_static/TO_Add_Commit_Push.sh
+++ web_static/TO_Add_Commit_Push.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-for file in ./*; do
-    if [ -f "$file" ]; then
-        filename=$(basename "$file")
-        message="Task ${filename%.*}"
-        git add "$file"
-        git commit -m "$message"
-    fi
-done
+# Add all new, modified, and deleted files to Git
+git add -A
 
-git push
+# Check if there are any changes to commit
+if git diff --quiet --cached; then
+    echo "No changes to commit."
+else
+    # Commit changes for all files
+    for file in $(git diff --name-only --cached); do
+        if [ -f "$file" ]; then
+            filename=$(basename "$file")
+            message="Task ${filename%.*}"
+            git commit -m "$message" "$file"
+        else
+            message="Deleted file: $file"
+            git commit -m "$message"
+        fi
+    done
+    
+    # Push changes to remote repository
+    git push
+    
+    echo "Changes committed and pushed to remote repository."
+fi
