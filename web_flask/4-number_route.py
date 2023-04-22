efdbf9a8507fae1fb5f26dd88eecb699e3698371
otomisin <orenaiket@yahoo.com>
2023-04-22 11:31:45 +0100

    __init__.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -38,6 +38,6 @@ def number(n):
     return '{:d} is a number'.format(n)
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
