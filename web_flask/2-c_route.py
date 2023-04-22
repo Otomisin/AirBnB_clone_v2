b907ca90de6a92e78a100128bc0e32e9c83b59fc
otomisin <orenaiket@yahoo.com>
2023-04-22 13:09:16 +0100

    Task 2-c_route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,22 +7,20 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """print web"""
+    """display web"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """print web"""
+    """display HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """print C followed by the value of the text variable"""
+    """display C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
-#Task
+    app.run(host='0.0.0.0', port=5000, debug=True)
