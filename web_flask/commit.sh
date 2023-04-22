a348f88b3f9a813a4dbf9f62b2dc2090cfa5a241
otomisin <orenaiket@yahoo.com>
2023-04-22 12:00:30 +0100

    14-commit.sh Task

--- web_flask/commit.sh
+++ web_flask/commit.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+task_number=1
+
 for file in ./*; do
     if [ -f "$file" ]; then
-        message=$(basename "$file")
+        message="$(basename "$file") Task"
         git add "$file"
-        git commit -m "$message"
+        git commit -m "$task_number-$message"
+        task_number=$((task_number+1))
     fi
 done
 
