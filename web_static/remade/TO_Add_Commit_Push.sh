64a03bed24641bd11f29524b03ecaa5e0b5efeac
otomisin <orenaiket@yahoo.com>
2023-04-22 13:59:31 +0100

    Task TO_Add_Commit_Push

--- web_static/remade/TO_Add_Commit_Push.sh
+++ web_static/remade/TO_Add_Commit_Push.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
-# Add all new, modified, and deleted files to Git
-git add -A
+for file in ./*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        message="Task ${filename%.*}"
+        git add "$file"
+        git commit -m "$message"
+    fi
+done
 
-# Check if there are any changes to commit
-if git diff --quiet --cached; then
-    echo "No changes to commit."
-else
-    # Commit changes for all files
-    for file in $(git diff --name-only --cached); do
-        if [ -f "$file" ]; then
-            filename=$(basename "$file")
-            message="$(echo ${filename%.*} | sed 's/-/ /g') Task"
-            git add "$file"
-            git commit -m "$message"
-        else
-            filename=$(basename "$file")
-            message="Deleted file: ${filename}"
-            git commit -m "$message"
-        fi
-    done
-    
-    # Push changes to remote repository
-    git push
-    
-    echo "Changes committed and pushed to remote repository."
-fi
+git push
