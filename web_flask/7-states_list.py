6800e546098a4619f670b0a9f9396528c5da994f
otomisin <orenaiket@yahoo.com>
2023-04-22 13:09:17 +0100

    Task 7-states_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -28,6 +28,5 @@ def states_list():
     return render_template("7-states_list.html", states=states)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
