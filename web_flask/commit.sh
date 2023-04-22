6449685ebe9260995aac16e7ee7aae8891374acb
otomisin <orenaiket@yahoo.com>
2023-04-22 11:53:16 +0100

    #!/bin/bash
    for file in ./*; do
        if [ -f "$file" ]; then
            message=$(cat "$file")
            git add "$file"
            git commit -m "$message"
        fi
    done
    
    git push

--- web_flask/commit.sh
+++ web_flask/commit.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Add all changes
-git add -A
-
-# Get the list of files that have been modified
-files=$(git diff --name-only)
-
-# Loop through each file and commit with its filename as the commit message
-for file in $files
-do
-  git commit -m "$file"
+for file in ./*; do
+    if [ -f "$file" ]; then
+        message=$(cat "$file")
+        git add "$file"
+        git commit -m "$message"
+    fi
 done
 
 git push
