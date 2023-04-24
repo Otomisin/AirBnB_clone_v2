1e28fdbe0b6d6b709d872f95977c85ffc3be6b4c
otomisin <orenaiket@yahoo.com>
2023-04-24 06:35:31 +0100

    update

--- commit.sh
+++ commit.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# add all new and modified files to the staging area
 git add .
-git status | grep 'new file\|modified' | grep -v 'appenv/' | awk '{print $2}' | xargs git commit -m "$1"
+
+# loop through the modified files
+for file in $(git diff --name-only --cached); do
+  # ignore files in the appenv/ directory
+  if [[ "$file" == appenv/* ]]; then
+    continue
+  fi
+  
+  # get the filename without the path and extension
+  filename=$(basename "$file" .$(echo "$file" | awk -F . '{print $NF}'))
+  # create the commit with the filename as the commit message
+  git commit -m "$filename" "$file"
+done
